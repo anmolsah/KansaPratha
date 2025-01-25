22574534972e03e6fbc9ef4ad3d48c61015987f5
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-25 22:20:29 +0530

    chore: update page title

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-3xl text-red-500 font-bold text-center'>hello world</div>
+    <div className='text-3xl text-red-500 font-bold text-center underline'>hello world</div>
   )
 }
 
